94025a40ca8bb5e9cb33e656f4574dc529bed331
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-08-30 23:31:17 +0530

    graphic added

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from cvzone.FaceDetectionModule import FaceDetector
 import pickle;
 import face_recognition;
 import numpy as np;
-# import cvzone
+import cvzone
+import os
 
 
 cap = cv.VideoCapture(0)
-cap.set(3, 600) #height
-cap.set(4, 520) #width
+cap.set(3, 640) #width
+cap.set(4, 480) #height
+imgBackground = cv.imread('resources/backgroundFinal2.png')
+
+# importing the mode images
+modePath = 'resources/modes'
+modePathList = os.listdir(modePath)
+imgModeList = []
+# print(modePathList)
+for path in modePathList:
+    imgModeList.append(cv.imread(os.path.join(modePath, path)))
+
+# print(len(imgModeList))
+
+
 detector = FaceDetector()
+
 
 # 2.graphic part
 
@@ -23,7 +38,7 @@ file.close()
 
 # 4.2 separate studentIds
 encodeListKnown, studentIds = encodeListKnownWithIds
-print(studentIds);
+print(studentIds)
 print("Encoded file load successfully...")
 
 face_names = []
@@ -31,12 +46,15 @@ face_names = []
 
 while True:
     success, img = cap.read()
-    # img, bboxs = detector.findFaces(img)
 
-    # if bboxs:
-    #     # bboxInfo - "id","bbox","score","center"
-    #     center = bboxs[0]["center"]
-    #     cv.circle(img, center, 5, (255, 0, 255), cv.FILLED)
+    imgBackground[162:162+480, 55:55+640] = img
+    imgBackground[35:35+640, 835:835+380] = imgModeList[1]
+    img, bboxs = detector.findFaces(img)
+
+    if bboxs:
+        # bboxInfo - "id","bbox","score","center"
+        center = bboxs[0]["center"]
+        cv.circle(img, center, 5, (255, 0, 255), cv.FILLED)
         
 
     # 4.3 image resizing to 1/4th
@@ -69,23 +87,28 @@ while True:
             # print(studentIds[matchIndex])
             name = studentIds[matchIndex]
             face_names.append(name)
-            # cvzone.cornerRect()
+
+            y1, x2, y2, x1 = faceLocation
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2- y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0) #rt = rectangle thickness
             
-            # Display the results
-            for (top, right, bottom, left), name in zip(faceCurrentFrame, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+            # # Display the results
+            # for (top, right, bottom, left), name in zip(faceCurrentFrame, face_names):
+            #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            #     top *= 4
+            #     right *= 4
+            #     bottom *= 4
+            #     left *= 4
 
-                # Draw a box around the face
-                cv.rectangle(img, (left, top), (right, bottom), (255, 0, 255), 1)
+            #     # Draw a box around the face
+            #     cv.rectangle(img, (left, top), (right, bottom), (255, 0, 255), 1)
 
-               # Draw a label with a name below the face
-                cv.rectangle(img, (left, bottom - 45), (right, bottom), (255, 0, 255), cv.FILLED)
-                font = cv.FONT_HERSHEY_DUPLEX
-                cv.putText(img, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            #    # Draw a label with a name below the face
+            #     cv.rectangle(img, (left, bottom - 45), (right, bottom), (255, 0, 255), cv.FILLED)
+            #     font = cv.FONT_HERSHEY_DUPLEX
+            #     cv.putText(img, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            
 
             
 
@@ -93,5 +116,6 @@ while True:
 
 
 
-    cv.imshow("Face Attendance", img)
+    # cv.imshow("Face Attendance", img)
+    cv.imshow("Face Attendance", imgBackground)
     cv.waitKey(1)
