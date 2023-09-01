c416f4f5730926dfb99b58b8aebd42dcab0387cb
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-09-01 12:28:52 +0530

    add db reference

--- database/connectToDb.py
+++ database/connectToDb.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
 
 # 5.1: db setup
@@ -8,3 +9,6 @@ firebase_admin.initialize_app(cred, {
     # 5.2: URL of realtime db
     "databaseURL" : "https://faceattendance-rtdb-default-rtdb.firebaseio.com/"
 })
+
+# 6.1: create reference of the db
+ref = db.reference("")
