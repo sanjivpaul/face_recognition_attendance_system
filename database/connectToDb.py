f49e15e18d8f89607f8181cf8fac2a1d57c145e6
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-09-01 13:33:40 +0530

    change image ratio

--- database/connectToDb.py
+++ database/connectToDb.py
@@ -7,8 +7,32 @@ from firebase_admin import db
 cred = credentials.Certificate("database/serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     # 5.2: URL of realtime db
-    "databaseURL" : "https://faceattendance-rtdb-default-rtdb.firebaseio.com/"
+    "databaseURL": "https://faceattendance-rtdb-default-rtdb.firebaseio.com/"
 })
 
 # 6.1: create reference of the db
-ref = db.reference("")
+ref = db.reference("Students")
+
+data = {
+    "MCA00922": {
+        "name": "Barkha Keshri",
+        "class_Roll_No": "MCA/009/22",
+        "session": "2022 - 2024",
+        "total_attendance": 13,
+        "year": 1,
+        "last_attendance_time": "2023-08-29 10:15:00"
+    },
+
+    "MCA02922": {
+        "name": "Sanjiv Paul",
+        "class_Roll_No": "MCA/029/22",
+        "session": "2022 - 2024",
+        "total_attendance": 12,
+        "year": 1,
+        "last_attendance_time": "2023-08-29 10:15:00"
+    },
+}
+
+# 6.2: send or insert data into db
+for key, value in data.items():
+    ref.child(key).set(value) 
